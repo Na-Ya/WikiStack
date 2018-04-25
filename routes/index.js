d2945d4d3051b85fc82907020fe9a262120879a4
Nabil Yafai <nabilyafai90@gmail.com>
2018-04-25 15:45:44 -0400

    added home redirect to wiki

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const wikiRouter = require('./wiki/wiki.js')
 const userRouter = require('./user/user.js')
 
 
-
+router.get('/', function(req, res, next){
+    res.redirect('/wiki')
+})
 router.use('/wiki', wikiRouter);
 router.use('/users',userRouter);
 
